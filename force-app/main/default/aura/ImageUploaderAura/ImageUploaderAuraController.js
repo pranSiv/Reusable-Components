8743eca6934a8eef7afde5473221945a79ef3be7
Parantap Srivastav <parantap.srivastav@logrhythm.com>
2020-04-23 17:12:35 -0700

    Success toast corrections

--- force-app/main/default/aura/ImageUploaderAura/ImageUploaderAuraController.js
+++ force-app/main/default/aura/ImageUploaderAura/ImageUploaderAuraController.js
@@ -16,16 +16,21 @@
             recId : component.get('v.recordId'),
             objectName : objectName
 		});
-
+        console.log('QQQQQQQQ');
 		action.setCallback(this, function(response){
-			var state = response.getState();
-			if(state === 'SUCCESS' || state === 'DRAFT') {
-				var resultToast = $A.get("e.force:showTest");
+            
+            var state = response.getState();
+            console.log(response.getReturnValue());
+			if(response.getReturnValue() == 'success') {
+                var resultToast = $A.get("e.force:showToast");
+                
 				resultToast.setParams({
-					"title" : "Success!",
-					"message" : uploadedFiles.length + "file uploadedsuccessfully!"
+					title: 'Success!',
+                    message: 'file uploaded successfully!',
+                    type: 'success'
 				});
-				resultToast.fire();;
+                resultToast.fire();
+                console.log('HAHAHAHAHAH');
 			}
 		});
 		$A.enqueueAction(action);
